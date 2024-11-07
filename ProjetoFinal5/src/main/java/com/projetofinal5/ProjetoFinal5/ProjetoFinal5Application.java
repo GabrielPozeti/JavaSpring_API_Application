e456db99df3528395726b6f530093fdd3ffda3fb
GabrielPozeti <161641369+GabrielPozeti@users.noreply.github.com>
2024-11-06 22:00:56 -0300

    Add files

---
+++ ProjetoFinal5/src/main/java/com/projetofinal5/ProjetoFinal5/ProjetoFinal5Application.java
@@ -0,0 +1,13 @@
+package com.projetofinal5.ProjetoFinal5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinal5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinal5Application.class, args);
+	}
+
+}
